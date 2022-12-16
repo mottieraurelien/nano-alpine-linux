efb0d42487fc4a5bdf9515699bbe508baea42a26
Aurelien MOTTIER <mottier.aurelien@gmail.com>
2022-12-16 10:36:54 +0800

    [Scripting] Samba (drafted before Japan and working fine)

--- samba/start.sh
+++ samba/start.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+#######################
+### 1/ REQUIREMENTS ###
+#######################
+
+# [Connected as root]
+if [ "$(whoami)" != "root" ]; then
+  echo "root privileges are required"
+  exit 1
+fi
+
+###############
+### 2/ MAIN ###
+###############
+
 # Update the packages list :
 apk update
-
-# Upgrade the packages :
-apk upgrade
 
 # Install the samba server :
 apk add samba
@@ -16,10 +27,20 @@ mkdir -p /media/ssd/
 rm -f /etc/samba/smb.conf
 cp smb.conf /etc/samba
 
-# Create the samba user that wants to read this folder :
-smbpasswd -a aurel
+# Define the new SMB user :
+echo "Input an username for the new SMB user : "
+IFS= read -r regularAccountUsername
+
+# Create the new SMB user that wants to read this folder :
+smbpasswd -a "$regularAccountUsername"
 # (a password prompt will pop up)
 
 # Run samba as a service :
 rc-update add samba
 rc-service samba start
+
+########################
+### 3/ HEALTH CHECKS ###
+########################
+
+exit 0
