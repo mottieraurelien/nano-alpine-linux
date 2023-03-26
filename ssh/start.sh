fc9f549ba10303973f8776cf5fe66ca8e0b1c36f
Aurelien MOTTIER <mottier.aurelien@gmail.com>
2023-03-26 19:30:02 +0800

    [Scripting] Setup procedure

--- ssh/start.sh
+++ ssh/start.sh
@@ -14,23 +14,16 @@ fi
 ### 2/ MAIN ###
 ###############
 
-# Check that the authorizes keys file contains one public key at least :
-numberOfAuthorizedKeys=$(cat "$AUTHORIZED_KEYS_FILE" | wc -l)
+# Check that his authorized keys file contains one public key at least :
+authorizedFile="/home/$REGULAR_USER/$(grep AuthorizedKeysFile /etc/ssh/sshd_config | cut -d$'\t' -f2)"
+numberOfAuthorizedKeys=$(cat "$authorizedFile" | wc -l)
 if [ "$numberOfAuthorizedKeys" -eq "0" ]; then
   echo "Please set up the key-based authentication before disabling the credentials-based authentication."
   exit
 fi
 
-# Prevent root from login (you can now connect through SSH with root, no need that login method anymore from host) :
-if grep -q "^PermitRootLogin yes" /etc/ssh/sshd_config; then
-  sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-fi
-if grep -q "^PermitRootLogin prohibit-password" /etc/ssh/sshd_config; then
-  sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
-fi
-if grep -q "^#PubkeyAuthentication yes" /etc/ssh/sshd_config; then
-  sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
-fi
+# Prevent using root when connecting to the server using SSH :
+sed -i 's/^PermitRootLogin.*/PermitRootLogin no/g'
 
 ########################
 ### 3/ HEALTH CHECKS ###
