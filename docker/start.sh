697f01e9904fcc44608d3a1c183fd0d928ba3d03
Aurelien MOTTIER <mottier.aurelien@gmail.com>
2023-03-26 19:58:35 +0800

    [Scripting] Setup docker procedure

--- docker/start.sh
+++ docker/start.sh
@@ -20,7 +20,7 @@ apk update
 # Install docker and docker-compose :
 apk add docker docker-compose
 
-# Define docker as a system service :
+# Define docker as a service (starts automatically when the server is starting) :
 rc-update add docker boot
 service docker start
 
@@ -30,6 +30,9 @@ addgroup "$REGULAR_USER" docker
 ########################
 ### 3/ HEALTH CHECKS ###
 ########################
+
+# Switch to the regular user to check its right:
+su - "$REGULAR_USER"
 
 # [docker has been successfully installed and regular account can run commands]
 if [ -z "$(command -v docker)" ]; then
@@ -41,5 +44,8 @@ if [ -z "$(command -v docker-compose)" ]; then
   echo "Docker-compose installation failed"
   exit 1
 fi
+
+# Leave the current regular account session:
+exit
 
 exit 0
