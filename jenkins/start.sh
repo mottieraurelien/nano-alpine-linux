d55adf6d0f16882fc7198a3df0bdbf9bff670dcd
Aurelien MOTTIER <mottier.aurelien@gmail.com>
2023-08-25 14:55:24 +0800

    [Scripting] Jenkins

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -17,9 +17,14 @@ fi
 # Shutdown Jenkins if exists :
 docker compose down
 rm -rf /pvs/jenkins
+deluser jenkins || true
 
-# Create the persistent volume :
-mkdir -p /pvs/jenkins/home
+# Create jenkins user so that we avoid using root, and add the user to docker group :
+adduser jenkins --disabled-password --no-create-home
+addgroup jenkins docker
+
+# Create the persistent volume and give ownership :
+mkdir -p /pvs/jenkins/home && chown -R jenkins:jenkins /pvs/jenkins
 
 # Run Jenkins :
 docker compose up --detach --force-recreate
